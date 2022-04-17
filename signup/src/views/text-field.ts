fab4d09a21629b13b2b0797ef8a01fdeba96a8fb
literate-t <gaekoon@gmail.com>
2022-04-17 20:30:17 +0900

    text-field.ts에서 update, render 살피는 중

--- signup/src/views/text-field.ts
+++ signup/src/views/text-field.ts
@@ -1,34 +1,36 @@
-import { nextTick } from '../utils';
-import { ValidateRule } from '../types';
-import template from './text-field.template';
-import { RequireRule } from '../constant';
+import { nextTick } from "../utils";
+import { ValidateRule } from "../types";
+import template from "./text-field.template";
+import { RequireRule } from "../constant";
 
 type Props = {
   id: string;
   label: string;
-  type: 'text' | 'email' | 'number';
+  type: "text" | "email" | "number";
   placeholder?: string;
   text?: string;
   require: boolean;
-}
+};
 
 const DefaultProps: Props = {
-  id: '',
-  text: '',
-  label: 'label',
-  type: 'text',
-  placeholder: '',
+  id: "",
+  text: "",
+  label: "label",
+  type: "text",
+  placeholder: "",
   require: false,
 };
 
 export default class TextField {
+  //  <div id="field-{{id}}"></div>
   private template = template;
   private container: string;
-  private data: Props;  
+  private data: Props;
   private updated: boolean = false;
   private validateRules: ValidateRule[] = [];
 
   constructor(container: string, data: Props) {
+    // this.container -> #required-fields
     this.container = container;
     this.data = { ...DefaultProps, ...data };
 
@@ -40,84 +42,90 @@ export default class TextField {
   }
 
   private validate = (): ValidateRule | null => {
-    const target = this.data.text ? this.data.text.trim() : '';
+    const target = this.data.text ? this.data.text.trim() : "";
 
-    const invalidateRules = this.validateRules
-      .filter(validateRule => validateRule.rule.test(target) !== validateRule.match);
+    const invalidateRules = this.validateRules.filter(
+      (validateRule) => validateRule.rule.test(target) !== validateRule.match
+    );
 
     // 검증 정책 위반이 여러 개 걸렸다고 해도 하나만 보여주기
-    return (invalidateRules.length > 0) ? invalidateRules[0] : null;
-  }
+    return invalidateRules.length > 0 ? invalidateRules[0] : null;
+  };
 
   private buildData = () => {
     const isInvalid: ValidateRule | null = this.validate();
 
     if (this.updated) {
       return {
-        ...this.data, 
+        ...this.data,
         updated: this.updated,
         valid: !isInvalid,
-        validateMessage: !!isInvalid ? isInvalid.message : ''
-      }
+        validateMessage: !!isInvalid ? isInvalid.message : "",
+      };
     } else {
       return {
-        ...this.data, 
+        ...this.data,
         updated: this.updated,
         valid: true,
-        validateMessage: ''
-      }
+        validateMessage: "",
+      };
     }
-  }
+  };
 
   private onChange = (e: Event) => {
     const { value, id } = e.target as HTMLInputElement;
-  
+
     if (id === this.data.id) {
       this.updated = true;
       this.data.text = value;
       this.update();
     }
-  }
+  };
 
   private attachEventHandler = () => {
-    document.querySelector(this.container)?.addEventListener('change', this.onChange);
-  }
+    document
+      .querySelector(this.container)
+      ?.addEventListener("change", this.onChange);
+  };
 
   private update = () => {
-    const container = document.querySelector(`#field-${this.data.id}`) as HTMLElement;
-    const docFrag = document.createElement('div');
+    const container = document.querySelector(
+      `#field-${this.data.id}`
+    ) as HTMLElement;
+    const docFrag = document.createElement("div");
 
     docFrag.innerHTML = this.template(this.buildData());
     // attachEventHandler에서 추가한 핸들러를 안 날리기 위해
+    // 핸들러는 버블링이 일어나므로 상관 없지 않나..
     container.innerHTML = docFrag.children[0].innerHTML;
-  }
+  };
 
   public get name(): string {
     return this.data.id;
   }
 
   public get value(): string {
-    return this.data.text || '';
+    return this.data.text || "";
   }
 
   public get isValid(): boolean {
     return !this.validate();
   }
 
-  public addValidateRule = (rule:ValidateRule) => {
+  public addValidateRule = (rule: ValidateRule) => {
     this.validateRules.push(rule);
-  }
+  };
 
   public render = (append: boolean = false) => {
     const container = document.querySelector(this.container) as HTMLElement;
 
     if (append) {
-      const divFragment = document.createElement('div');
+      const divFragment = document.createElement("div");
       divFragment.innerHTML = this.template(this.buildData());
 
       container.appendChild(divFragment.children[0]);
     } else {
       container.innerHTML = this.template(this.buildData());
     }
-  }
+  };
 }
